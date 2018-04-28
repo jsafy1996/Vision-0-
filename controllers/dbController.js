a90280dc7009673cc6bb4c8a965000336c855f73
Maria Volpe <mariaCHVolpe@gmail.com>
2018-04-28 17:28:20 -0400

    Minor changes

--- controllers/dbController.js
+++ controllers/dbController.js
@@ -1,11 +1,26 @@
 //do stuff
-
+function calcRoute() {
+    var start = document.getElementById('start').value;
+    var end = document.getElementById('end').value;
+    var request = {
+      origin: start,
+      destination: end,
+      travelMode: 'DRIVING'
+    };
+    directionsService.route(request, function(result, status) {
+      if (status == 'OK') {
+        directionsDisplay.setDirections(result);
+      }
+    });
+  }
+  
 //routing
 module.exports = function(serve){
 
+
 //serve index page
 serve.get("/", function(req, res){
-    res.render("index");
+    res.render("index2");
 });
 
 serve.get("/other", function(req, res){
