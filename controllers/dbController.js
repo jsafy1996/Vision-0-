cdbe0219e9be43a02abc5fa4db8f63b62ba3aace
John Safy <jsafy1996@gmail.com>
2018-04-28 17:26:28 -0400

    Markers function

--- controllers/dbController.js
+++ controllers/dbController.js
@@ -5,11 +5,11 @@ module.exports = function(serve){
 
 //serve index page
 serve.get("/", function(req, res){
-    res.render("index");
+    res.render("index2");
 });
 
 serve.get("/other", function(req, res){
-    res.render("index");
+    res.render("index2");
 });
 
   
