e696f69a4f9e41b943c4d791465f24eb47662d05
Maria Volpe <mariaCHVolpe@gmail.com>
2018-04-28 11:30:58 -0400

    Add map views and fix syntax errors

--- serve.js
+++ serve.js
@@ -1,18 +1,18 @@
 var express = require("express");
-//var controller1 = require("");
+var dbController = require("./controllers/dbController");
 
 var serve = express();
 
 //set up ejs as template engine
-//app.set("view engine", "ejs");
+serve.set("view engine", "ejs");
 
 //serve static files (css styles)
-serve.use(express.static("./assets"));
+//serve.use(express.static("./assets"));
 
 //fire controllers
-controller1(serve);
+dbController(serve);
 
 //listen to port
-app.listen(3000);
+serve.listen(3000);
 
 module.exports = serve;
