ac64b2e8454b61c990340c3df45a7a0152336151
Maria Volpe <mariaCHVolpe@gmail.com>
2018-04-28 21:49:08 -0400

    Add function to add fields to DB

--- flaskbackend/app.py
+++ flaskbackend/app.py
@@ -1,17 +1,24 @@
 from flask import Flask, render_template, jsonify
 import json
+import data_processing
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return render_template('index2.html')
 
+@app.route('/2')
+def hello():
+    return render_template('index.html')
+
+
 @app.route('/do/<coords>')
 def doStuff(coords):
     print(coords)
-    #print(json.loads(coords))
-
-    return jsonify({'coords':'hello world'})
+    #data_processing.fix_database()
+    #data_processing.stuff(coords)
+    return coords
 
 if(__name__ == '__main__'):
     app.run(debug=True)
